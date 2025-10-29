758918e4814a2500436e8ba61730c3939b0a0222
StarJ <ghdtjdwo126@gmail.com>
2025-10-29 16:14:34 +0900

    builder CD.yml 및 deploy 변경

--- builder/deploy.sh
+++ builder/deploy.sh
@@ -4,15 +4,28 @@ set -e # 명령어 실행 중 오류 발생 시 즉시 종료
 # sudo chown root:root /home/아이디/deploy
 # sudo chmod 555 /home/아이디/deploy.sh
 
-LOC=/home/아이디
-# 1. Docker 이미지 로드 및 삭제
-docker load -i "${LOC}/app_image.tar"
-rm -f "${LOC}/app_image.tar"
+# 0. 환경변수 설정
+HOME="$1"
+IMAGE_FULL_TAG="$2"
+DOCKER_USERNAME="$3"
+REGISTRY_URL="$4"
+DOCKER_TOKEN="$5"
+CONTAINER_NAME="$6"
+VOLUME_PATH="$7"
 
-# 2. 기존 컨테이너 중지 및 제거
-CONTAINER_NAME="builder"
+#1. Docker 로그인
+echo "${DOCKER_TOKEN}" | sudo docker login ${REGISTRY_URL} -username "${DOCKER_USERNAME}" --password-stdin
 
-# 실행 중인 컨테이너가 있으면 중지 및 제거
+#2. Docker pull
+sudo docker pull "${IMAGE_FULL_TAG}"
+
+#3. Dokcer 로그아웃
+sudo docker logout ${REGISTRY_URL}
+#4. 기존 폴더 삭제 및 재생성
+sudo rm -rf ${VOLUME_PATH}
+sudo mkdir -p ${VOLUME_PATH}
+
+#5. 기존 컨테이너 중지 및 제거
 if docker ps -qf "name=${CONTAINER_NAME}" | grep -q .; then
     docker stop "${CONTAINER_NAME}"
     docker rm -f "${CONTAINER_NAME}"
@@ -23,11 +36,8 @@ else
     fi
 fi
 
-# 4. 새 컨테이너 실행
-# 볼륨 경로를 변수로 명확히 사용
-VOLUME_PATH="${LOC}/builder"
-
+#6. 새 컨테이너 실행
 docker run -d \
     -v "${VOLUME_PATH}":/share \
     --name "${CONTAINER_NAME}" \
-    local-app:latest
+    "${IMAGE_FULL_TAG}"
